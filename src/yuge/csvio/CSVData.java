43dbaf0504d1df3e941f382ab884f06950ef3492
NakagamiYuta <guguru0014@gmail.com>
2020-01-17 11:32:38 +0900

    [add] CSVDataクラス定義

---
+++ src/yuge/csvio/CSVData.java
@@ -0,0 +1,17 @@
+package yuge.csvio;
+
+public void CSVData {
+
+    public String contest, name;
+    public double backstroke, braststroke, butterfly, free;
+
+    public CSVData(String contest, String name, double backstroke, double braststroke, double butterfly, double free) {
+        this.contest = contest;
+        this.name = name;
+        this.backstroke = backstroke;
+        this.braststroke = braststroke;
+        this.butterfly = butterfly;
+        this.free = free;
+    }
+
+}
