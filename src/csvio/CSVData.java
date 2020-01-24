fc2fc2a0ce674e5656d568338982222d2a983df1
NakagamiYuta <guguru0014@gmail.com>
2020-01-24 10:14:00 +0900

    [remove] CSVDataのsizeメンバを削除

--- src/csvio/CSVData.java
+++ src/csvio/CSVData.java
@@ -2,13 +2,7 @@ package csvio;
 
 public abstract class CSVData {
 
-    protected int size;
-
     abstract public void setData(Object ... args);
-
-    public int getSize() {
-        return size;
-    }
 
     protected boolean checkArgsSize(Object args[], int checkSize) {
         return args.length != checkSize;
