45c296313ec84bf9afe7900f55e555c59d847716
NakagamiYuta <guguru0014@gmail.com>
2020-01-24 10:23:45 +0900

    [add] CSVDataにjavadocコメントを追加

--- src/csvio/CSVData.java
+++ src/csvio/CSVData.java
@@ -2,10 +2,21 @@ package csvio;
 
 public abstract class CSVData {
 
+    /**
+     * データセット
+     *
+     * @param args 登録するデータを渡す(可変長引数)
+     */
     abstract public void setData(Object ... args);
 
-    protected boolean checkArgsSize(Object args[], int checkSize) {
-        return args.length != checkSize;
+    /**
+     * 引数サイズをチェックする
+     *
+     * @param args 引数の配列
+     * @param checkSize サイズ
+     */
+    protected boolean checkArgsSize(Object args[], int size) {
+        return args.length != size;
     }
 
 }
