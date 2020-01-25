91b408c7baeabecf0f3359cf37fe9a2b9c2f380a
NakagamiYuta <guguru0014@gmail.com>
2020-01-25 23:01:12 +0900

    [remove] makeCSVDataArray削除

--- src/simulator/SwimSimulator.java
+++ src/simulator/SwimSimulator.java
@@ -1,15 +1,12 @@
 package simulator;
 
 import csvio.format.SwimData;
-import static csvio.util.makeCSVDataArray;
 
 public class SwimSimulator {
 
     SwimData data[];
 
-
     public SwimSimulator() {
-        data = makeCSVDataArray(SwimData.class, 4);
     }
 
 }
