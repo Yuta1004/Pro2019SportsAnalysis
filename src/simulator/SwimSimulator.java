2241bc89593d1bf8a2fecb3759f755818204581d
NakagamiYuta <guguru0014@gmail.com>
2020-01-26 20:45:22 +0900

    [add] 各泳法のMETsを定数としてSwimSimulatorに追加

--- src/simulator/SwimSimulator.java
+++ src/simulator/SwimSimulator.java
@@ -7,7 +7,12 @@ import java.util.ArrayList;
 
 public class SwimSimulator {
 
-    ArrayList<SwimData> data;
+    private static final double BUTTERFLY_METs = 13.5;
+    private static final double BRASTSTROKE_METs = 12.0;
+    private static final double BACKSTROKE_METs = 9.0;
+    private static final double FREE_METs = 13.0;
+
+    private ArrayList<SwimData> data;
 
     public SwimSimulator(String csvPath) {
         CSVReader reader = new CSVReader(csvPath);
