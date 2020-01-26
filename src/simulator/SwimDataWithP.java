59d6260821d05e284808b9a3d428605317f92da4
NakagamiYuta <guguru0014@gmail.com>
2020-01-26 22:04:31 +0900

    [add] SwimDataWithPにメンバpを追加した

--- src/simulator/SwimDataWithP.java
+++ src/simulator/SwimDataWithP.java
@@ -5,5 +5,14 @@ import csvio.format.SwimData;
 public class SwimDataWithP extends SwimData {
 
     public double maxCalorie;
+    public double p[];
+
+    // 0 -> バタフライ
+    // 1 -> 背泳ぎ
+    // 2 -> 平泳ぎ
+    // 3 -> 自由形
+    public SwimDataWithP() {
+        p = new double[4];
+    }
 
 }
