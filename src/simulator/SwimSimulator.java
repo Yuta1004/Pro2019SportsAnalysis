966816108f28ea0bab233bd0f8a16b21887c3183
NakagamiYuta <guguru0014@gmail.com>
2020-01-26 21:22:30 +0900

    [add] 最大消費カロリー算出処理を書いた

--- src/simulator/SwimSimulator.java
+++ src/simulator/SwimSimulator.java
@@ -19,19 +19,30 @@ public class SwimSimulator {
      * @param csvPath CSVファイルのパス
      */
     public SwimSimulator(String csvPath) {
+        // CSVファイル読み込み
         CSVReader reader = new CSVReader(csvPath);
         data = reader.load(SwimDataWithP.class);
+
+        // 最大消費カロリー計算
+        for(SwimDataWithP elem : data) {
+            double maxCalorie = 0.0;
+            maxCalorie += calcCalorie(BUTTERFLY_METs, elem.butterfly, elem.weight);
+            maxCalorie += calcCalorie(BRASTSTROKE_METs, elem.braststroke, elem.weight);
+            maxCalorie += calcCalorie(BACKSTROKE_METs, elem.backstroke, elem.weight);
+            maxCalorie += calcCalorie(FREE_METs, elem.free, elem.weight);
+            elem.maxCalorie = maxCalorie;
+        }
     }
 
     /**
      * 消費カロリー計算
      *
      * @param mets METs
-     * @param time 運動時間
+     * @param time 運動時間(s)
      * @param weight 体重
      */
     private double calcCalorie(double mets, double time, double weight) {
-        return mets * time * weight * 1.05;
+        return mets * (time/3600.0) * weight * 1.05;
     }
 
 }
