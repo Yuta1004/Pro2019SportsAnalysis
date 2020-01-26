cb01c3418ab7dc61c8513234b31725eb99d1f095
NakagamiYuta <guguru0014@gmail.com>
2020-01-26 23:11:22 +0900

    [add] SwimSimulatorTestとりあえず書いた

--- src/test/SwimSimulatorTest.java
+++ src/test/SwimSimulatorTest.java
@@ -3,6 +3,9 @@ package test;
 import csvio.reader.CSVReader;
 import simulator.SwimDataWithSimulator;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class SwimSimulatorTest extends Test {
 
     /**
@@ -11,8 +14,22 @@ public class SwimSimulatorTest extends Test {
     @Override
     public void doTest() {
         CSVReader reader = new CSVReader("src/data/swim.csv");
-        reader.load(SwimDataWithSimulator.class);
-        test("TestConstract", true, true);
+        ArrayList<SwimDataWithSimulator> data = reader.load(SwimDataWithSimulator.class);
+
+        Random rand = new Random();
+        double p1 = rand.nextDouble() * 1.9 + 0.1;
+        double p2 = rand.nextDouble() * 1.9 + 0.1;
+        double p3 = rand.nextDouble() * 1.9 + 0.1;
+        double p4 = rand.nextDouble() * 1.9 + 0.1;
+
+        int select = 0;
+        SwimDataWithSimulator player = data.get(select);
+        player.setPValue(p1, p2, p3, p4);
+
+        System.out.println("Name: "+player.name);
+        System.out.println("Time: "+player.getTime());
+        System.out.println("Calorie: "+player.getCalorie());
+        System.out.println("PValues: "+p1+" ,"+p2+", "+p3+", "+p4);
     }
 
 }
