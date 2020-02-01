5510c8a7d1fb6627b7d5bfa91777a2999529ff20
NakagamiYuta <guguru0014@gmail.com>
2020-02-01 18:03:23 +0900

    [add] ExecuteTestにRouletteTestを追加

--- src/ExecuteTest.java
+++ src/ExecuteTest.java
@@ -1,13 +1,15 @@
 import test.Test;
 import test.CSVReaderTest;
 import test.SwimSimulatorTest;
+import test.RouletteSelectTest;
 
 class ExecuteTest {
 
     public static void main(String[] args) {
         Test allTests[] = {
             new CSVReaderTest(),
-            new SwimSimulatorTest()
+            new SwimSimulatorTest(),
+            new RouletteSelectTest()
         };
 
         for(Test test: allTests) {
