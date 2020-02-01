ec181e6c624a48f535fe138cde5080fd35a19338
NakagamiYuta <guguru0014@gmail.com>
2020-02-01 17:18:40 +0900

    [add] Pairクラスにjavadocコメント追加

--- src/util/Pair.java
+++ src/util/Pair.java
@@ -7,15 +7,24 @@ public class Pair<K, V extends Comparable> implements Comparable{
     public K first;
     public V second;
 
+    /**
+     * Pairのコンストラクタ
+     *
+     * @param first  要素1
+     * @param second 要素2
+     */
     public Pair(K first, V second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * 2要素の比較
+     */
     public int compareTo(Object other) {
         Pair otherPair = (Pair)other;
         return second.compareTo(otherPair.second);
     }
 
-} 
+}
 
