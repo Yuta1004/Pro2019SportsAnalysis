62eabc2d1cf7f8e4d4c11d0b43b01cbad9d22dbf
NakagamiYuta <guguru0014@gmail.com>
2020-02-01 17:58:52 +0900

    [add] RouletteSelectTest

---
+++ src/test/RouletteSelectTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import simulator.RouletteSelect;
+
+public class RouletteSelectTest extends Test {
+
+    /**
+     * テストケース実行(Testの抽象メソッド)
+     */
+    @Override
+    public void doTest() {
+        double numbers[] = {1, 2, 5, 6, 8, 9, 4, 1, 1, 0};
+        RouletteSelect selector = new RouletteSelect(numbers);
+    }
+
+}
+
